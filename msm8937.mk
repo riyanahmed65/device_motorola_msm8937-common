2d36d367d78cbcf2201f972ec8a2f6a129b39bb1
Erfan Abdi <erfangplus@gmail.com>
2018-09-25 19:20:49 +0300

    msm8937-common: Port marlin 1.0 vibrator HAL
    Change-Id: Id3f891afd3dab8ece41002056a462a49ca3c851f
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>

--- msm8937.mk
+++ msm8937.mk
@@ -393,7 +393,7 @@ PRODUCT_PACKAGES += \
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    android.hardware.vibrator@1.0-service.msm8937
 
 # Wifi
 PRODUCT_PACKAGES += \

a218a808b1bf996ec75b143b4365faf37b2a3097
Ryan Mitchell <rtmitchell@google.com>
2019-10-10 10:21:46 -0700

    msm8937-common: Use component-override to configure Bluetooth
    Runtime Resource Overlays (RROs) can no longer change the value of
    resources in manifest when read during PackagerParser.
    
    This change uses component-override to enable the service by default
    on all devices.
    
    Remove usages of profile_supported_sap from overlays as they no
    longer configure the enable state of .sap.SapService.
    
    Bug: 135048762
    Test: adb shell pm query-services \
          com.android.bluetooth/.sap.SapService
    
    Change-Id: If41df9fdefbcd952f8387451dc137565343e3d75

--- msm8937.mk
+++ msm8937.mk
@@ -69,6 +69,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+
 # Boot
 PRODUCT_BUILD_BOOT_IMAGE := true
 

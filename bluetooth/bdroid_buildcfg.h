66f1788975c3593cc6edfd419ad4e9b6241a68f9
Rohan Hasabe <rohanhasabe8@gmail.com>
2020-12-17 14:34:29 +0100

    msm8937-common: Add jeter to build guards
    Change-Id: If24f370f18cbc00a7a9bfb0681fdaf1cf0dcdf8e

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -39,6 +39,7 @@ static const device_t devices[] = {
     {"cedric", "moto g5"},
     {"hannah", "moto e5 plus"},
     {"james", "moto e5 play"},
+    {"jeter", "moto g6 play"},
     {"montana", "moto g5s"},
 };
 

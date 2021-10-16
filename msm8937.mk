302983360fa1c526d45a78d67a0e4c421d594ea8
Jarl-Penguin <jarlpenguin@outlook.com>
2021-10-16 10:35:14 +0300

    msm8937-common: Define default certificate
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- msm8937.mk
+++ msm8937.mk
@@ -24,6 +24,11 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Signing
+ifneq "$(wildcard vendor/certs )" ""
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/certs/releasekey
+endif
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
